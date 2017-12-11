9bb082ad1eaf4863a054377e71039fffaccdf28a
Dylan Hanner <dylan.hanner@yahoo.co.uk>
2017-12-11 20:07:21 +0000

    Added basic ui-navigation controllers and placeholder text for the site

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
-var app = angular.module('luciadesign')
+var app = angular.module('luciadesign',['ui.router'])
+
+app.run(function($rootScope) {
+
+})
+
+app.config(function($stateProvider, $urlRouterProvider) {
+	$stateProvider
+		.state('main',{
+			url:'/main',
+			templateUrl: '/templates/main.html',
+			controller: 'navCtrl'
+		})
+
+	$urlRouterProvider
+		.otherwise('/main')
+})
