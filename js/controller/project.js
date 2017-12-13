abc1202c7aab401ae5215db1ca781a4663d9a68c
Dylan Hanner <dylan.hanner@yahoo.co.uk>
2017-12-13 20:43:36 +0000

    basic image browser added

--- js/controller/project.js
+++ js/controller/project.js
@@ -1,6 +1,33 @@
 app.controller('project', function($scope, $state, projects) {
 	$scope.project = projects.get( $state.params.anchor )
 
+	$scope.show_nav_right = true
+	$scope.show_nav_left  = false
+	$scope.selected_image = 0
+
+	$(document).ready(sizeImages)
+	$(window).resize(sizeImages)
+
+	$scope.navRight = function() {
+		var wrapper   = $('.images div.wrapper')
+		var images    = $('.image-container')
+		var scroll    = wrapper[0].scrollLeft + wrapper[0].scrollWidth / images.length
+		wrapper[0].scroll({ left:scroll, top:0, behavior:'smooth' })
+		$scope.selected_image++
+		$scope.show_nav_left  = $scope.selected_image > 0
+		$scope.show_nav_right = $scope.selected_image < images.length - 1
+	}
+
+	$scope.navLeft = function() {
+		var wrapper   = $('.images div.wrapper')
+		var images    = $('.image-container')
+		var scroll    = wrapper[0].scrollLeft - wrapper[0].scrollWidth / images.length
+		wrapper[0].scroll({ left:scroll, top:0, behavior:'smooth' })
+		$scope.selected_image--
+		$scope.show_nav_left  = $scope.selected_image > 0
+		$scope.show_nav_right = $scope.selected_image < images.length - 1
+	}
+
 	$scope.scrollTop = function() {
 		$(document).ready(function() {
 			$('.page-container')[0].scroll({top:0,left:0,behavior:'smooth'})
@@ -12,6 +39,12 @@ app.controller('project', function($scope, $state, projects) {
 			var scrollHeight = $('.page-container')[0].scrollHeight
 			$('.page-container')[0].scroll({top:scrollHeight,left:0,behavior:'smooth'})
 		})
+	}
+
+	function sizeImages() {
+		var image_width = $('.images div.wrapper').width()
+		$('.image-container').css({ width: image_width})
+		$('.images-container').css({ width: image_width*$scope.project.images.length})
 	}
 
 	function setBottom() {
